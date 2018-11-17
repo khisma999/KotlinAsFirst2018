288573b654bf036360d887fe51bd4affcfe114a9
khisma999 <leniza_khismatullina@mail.ru>
2018-11-17 21:32:21 +0300

    пятый урок (1)

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -214,7 +214,7 @@ fun subtractOf(a: MutableMap<String, String>, b: Map<String, String>): Unit {
  *
  * Для двух списков людей найти людей, встречающихся в обоих списках
  */
-fun whoAreInBoth(a: List<String>, b: List<String>): List<String> = a.filter{b.contains(it)}
+fun whoAreInBoth(a: List<String>, b: List<String>): List<String> = a.filter{it in b}
 
 /**
  * Средняя
